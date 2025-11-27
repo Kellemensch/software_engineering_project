51ea177ac3938ab7430c9304ca34fda793b0c04f
desktery <214663839+desktery@users.noreply.github.com>
2025-11-27 13:34:44 +0100

    linked Review with the model

--- src/Review.tsx
+++ src/Review.tsx
@@ -1,20 +1,20 @@
+import { useEffect, useState } from "react";
+import { getReceipts, type Receipt } from "./model/receipt";
 import "./Review.css";
 import { Info, CheckCircle } from "lucide-react";
+import { formatDate } from "./utils";
 
 export default function ReviewReceipt() {
-    const notifications = [
-        { account: "Liam Neeson", date: "1.1.2025", store: "Lidl", status: "new" },
-        { account: "Dwayne Johnson", date: "31.12.2024", store: "Lidl", status: "new" },
-        { account: "Katy Perry", date: "12.11.2024", store: "Lidl", status: "accepted" },
-        { account: "Dwayne Johnson", date: "1.1.2024", store: "Lidl", status: "accepted" },
-        { account: "Dwayne Johnson", date: "1.1.2025", store: "Lidl", status: "accepted" },
-        { account: "Liam Neeson", date: "31.12.2024", store: "Lidl", status: "accepted" },
-        { account: "Dwayne Johnson", date: "12.11.2024", store: "Lidl", status: "accepted" },
-        { account: "Katy Perry", date: "1.1.2024", store: "Lidl", status: "accepted" },
-    ];
+    const [receipts, setReceipts] = useState<Receipt[]>([]);
 
-    const waitingNotifications = notifications.filter(n => n.status === "new");
-    const managedNotifications = notifications.filter(n => n.status === "accepted" || n.status === "rejected");
+    useEffect(() => {
+        setReceipts(getReceipts());
+    }, []);
+
+    const waitingNotifications = receipts.filter((n) => n.status === "new");
+    const managedNotifications = receipts.filter(
+        (n) => n.status === "approved" || n.status === "rejected",
+    );
 
     return (
         <div className="dashboard-container">
@@ -22,8 +22,16 @@ export default function ReviewReceipt() {
                 <h2>Waiting</h2>
                 <div className="notifications-list">
                     {waitingNotifications.map((n, i) => (
-                        <div key={i} className={`notification-item ${n.status === "new" ? "new" : ""} ${n.status === "rejected" ? "rejected" : ""}`}>
-                            <span>{n.account}<br />{n.date} – {n.store}</span>
+                        <div
+                            key={i}
+                            className={`notification-item ${n.status === "new" ? "new" : ""} ${n.status === "rejected" ? "rejected" : ""}`}
+                        >
+                            <span>
+                                {n.salesperson.firstname}{" "}
+                                {n.salesperson.lastname}
+                                <br />
+                                {formatDate(n.date)} – {n.title}
+                            </span>
                             <Info size={24} className="icon info-icon" />
                         </div>
                     ))}
@@ -34,9 +42,20 @@ export default function ReviewReceipt() {
                 <h2>Managed</h2>
                 <div className="notifications-list">
                     {managedNotifications.map((n, i) => (
-                        <div key={i} className={`notification-item ${n.status === "new" ? "new" : ""} ${n.status === "rejected" ? "rejected" : ""}`}>
-                            <span>{n.account}<br />{n.date} – {n.store}</span>
-                            <CheckCircle size={24} className="icon accepted-icon" />
+                        <div
+                            key={i}
+                            className={`notification-item ${n.status === "new" ? "new" : ""} ${n.status === "rejected" ? "rejected" : ""}`}
+                        >
+                            <span>
+                                {n.salesperson.firstname}{" "}
+                                {n.salesperson.lastname}
+                                <br />
+                                {formatDate(n.date)} – {n.title}
+                            </span>
+                            <CheckCircle
+                                size={24}
+                                className="icon accepted-icon"
+                            />
                         </div>
                     ))}
                 </div>
